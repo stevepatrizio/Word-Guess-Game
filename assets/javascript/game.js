ecf4cdf5d5cfe25708a43c31bb9a24020d655e03
stevepatrizio <56764802+stevepatrizio@users.noreply.github.com>
2020-01-11 15:35:08 -0500

    Added code to push user choice to HTML

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,26 +12,35 @@ var userGuess = '';
 var userWins = 0;
 var userLosses = 0;
 var totalGuesses = 0;
-var myIndex = 0;
-var myIndexOf = 0;
-
+var compGuess = '';
+var psychicWord = document.getElementById("psychicWord");
  
 //This function selects a random array index value from var myPsychic[]
 function getPsycgicWord() {
-        var random = Math.floor(Math.random()*(myPsychic.length-1));
-        //document.write(random);
-        document.write(myPsychic[random]);
+    var random = Math.floor(Math.random()*(myPsychic.length-1));
+    //Maps randon index value to a specific value in the array (band)
+    //Then writes value (band) to the HTML
+    psychicWord.textContent = myPsychic[random];
+    console.log(myPsychic[random]);
+    return myPsychic[random];
+}
+
+compGuess  = getPsycgicWord();
+
+document.onkeyup = function(event) {
+    // Determines which key was pressed.
+    userGuess = event.key;
+    //console.log(userGuess);    
+        
+    for (i=0; i < compGuess.length; i++){
+
+        if(userGuess === compGuess[i]){
+            // console.log("hello");
+            console.log(compGuess[i]);
+        }           
     }
+}
 
-    document.onkeyup = function(event) {
-
-        // Determines which key was pressed.
-        var userGuess = event.key;
-        console.log(userGuess);
-    }
-
-    getPsycgicWord();
-
-
+    
 
 
