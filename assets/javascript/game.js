8a9245bdf06e838dc275e2e49e77692d82d44d6d
stevepatrizio <56764802+stevepatrizio@users.noreply.github.com>
2020-01-12 09:22:16 -0500

    Added code to push user choice to HTML

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,18 +14,32 @@ var userLosses = 0;
 var totalGuesses = 0;
 var compGuess = '';
 var psychicWord = document.getElementById("psychicWord");
+var blankWords = document.getElementById("blankWords");
+var vBlanks = '';
  
 //This function selects a random array index value from var myPsychic[]
 function getPsycgicWord() {
     var random = Math.floor(Math.random()*(myPsychic.length-1));
-    //Maps randon index value to a specific value in the array (band)
-    //Then writes value (band) to the HTML
+    //Maps random index value to a specific value in the array (band)
+    //Then writes that value (band) to the HTML
     psychicWord.textContent = myPsychic[random];
-    console.log(myPsychic[random]);
+    // document.getElementById("blankWords").innerHTML = pos;
+    
+    console.log("This is my function myPsychic value: " + myPsychic[random]);
     return myPsychic[random];
 }
 
+vBlanks = myBlanks();
 compGuess  = getPsycgicWord();
+
+function myBlanks() {
+            for (var i = 0; i < compGuess.length; i++){
+                
+                vBlanks[i] = "_ ";
+                blankWords.textContent = myPsychic[i];
+                console.log(blankWords); 
+                }
+}
 
 document.onkeyup = function(event) {
     // Determines which key was pressed.
@@ -34,10 +48,14 @@ document.onkeyup = function(event) {
         
     for (i=0; i < compGuess.length; i++){
 
+        // vBlanks[i] = "_ ";
+        // blankWords.textContent = vBlanks;
+        // console.log(blankWords);
+
         if(userGuess === compGuess[i]){
             // console.log("hello");
             console.log(compGuess[i]);
-        }           
+        } 
     }
 }
 
